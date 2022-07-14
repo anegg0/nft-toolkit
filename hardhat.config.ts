76790859b54f8ca56f7dfd2dd03da2e0c83cf4e9
anegg0 <gael.blanchemain@gmail.com>
2022-07-14 14:53:25 -0700

    Added Localhost network to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import { utils } from "ethers";
+import * as dotenv from "dotenv";
+dotenv.config();
+const privateKey = process.env.PRIVATE_KEY;
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      accounts: [
+        {
+          privateKey: privateKey as string,
+          balance: utils.parseUnits("1", 36).toString(),
+        },
+      ],
+    },
+  },
 };
 
 export default config;
