1e5bdd0230bcecff87ec1a970e461595cfb4ac5d
anegg0 <gael.blanchemain@gmail.com>
2022-07-15 10:28:51 -0700

    Added clearer console.log message upon deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,14 +3,10 @@ import { ethers } from "hardhat";
 async function main() {
   const NFT = await ethers.getContractFactory("NFT");
   const nft = await NFT.deploy();
-
   await nft.deployed();
-
-  console.log("NFT:", nft.address);
+  console.log("NFT contract deployed to:", nft.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
