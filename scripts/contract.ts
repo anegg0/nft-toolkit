685ddff8eeeb89455d6d6d87fad5e3d5a3a71cbf
anegg0 <gael.blanchemain@gmail.com>
2022-07-15 12:53:51 -0700

    Hard-coded deployed contract address

--- scripts/contract.ts
+++ scripts/contract.ts
@@ -3,7 +3,7 @@ import { getContractAt } from "@nomiclabs/hardhat-ethers/internal/helpers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import * as dotenv from "dotenv";
 dotenv.config();
-const deployedContratAddress: any = process.env.CONTRACT_ADDRESS;
+const deployedContratAddress: any = "0x25d9B767261b66666B7Eec3becd17eEde75D07A7";
 
 export async function getContract(
   { name, hre }: { name: string; hre: HardhatRuntimeEnvironment; }): Promise<Contract> {
