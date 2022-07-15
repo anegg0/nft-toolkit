6f9e5cf25a87693e670a527e8f2431a96014f987
anegg0 <gael.blanchemain@gmail.com>
2022-07-14 17:36:35 -0700

    Added mint testing script

--- test/NFT.ts
+++ test/NFT.ts
@@ -6,7 +6,6 @@ describe("NFT", function () {
 
     async function deployFixture() {
         const [owner, otherAccount] = await ethers.getSigners();
-
         const NFT = await ethers.getContractFactory("NFT");
         const nft = await NFT.deploy();
         return { nft, owner, otherAccount };
@@ -16,5 +15,14 @@ describe("NFT", function () {
         const { nft, owner } = await loadFixture(deployFixture);
 
         expect(await nft.owner()).to.equal(owner.address);
+    });
+
+    it("Should mint an NFT with the right tokenURI", async function () {
+        const { nft, owner } = await loadFixture(deployFixture);
+        let tokenURI = "ipfs://bafkreihm63mue4z3qewc6nhj7ctzaediburwtu2ydr5iid26wgnilbnyhu";
+        await nft.safeMint(owner.address, tokenURI);
+        expect(await nft.tokenURI(0)).to.equal(
+            "ipfs://bafkreihm63mue4z3qewc6nhj7ctzaediburwtu2ydr5iid26wgnilbnyhu"
+        );
     });
 });
