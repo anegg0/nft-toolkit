36e1451c50f86c27070db615f0d6dbab6249d889
mbaxter <meredith.baxter@palm.io>
2022-08-08 16:52:16 -0400

    "Develop contract" review fixes (#6)
    * Fix minting tests
    
    - Set tx sender correctly
    - Fix assertions related to success
    - Add checks for token balance
    
    * Update package name in lock file

--- test/NFT.ts
+++ test/NFT.ts
@@ -18,15 +18,29 @@ describe("NFT", function () {
         expect(await nft.owner()).to.equal(owner.address);
     });
 
-    it("Should mint succesfully if owner is the one minting", async function () {
-        const { nft, owner } = await loadFixture(deployFixture);
-        const tokenURI: any = "ipfs://bafkreihm63mue4z3qewc6nhj7ctzaediburwtu2ydr5iid26wgnilbnyhu";
-        expect(await nft.safeMint(owner.address, tokenURI)).to.not.throw;
+    it("Should mint successfully if owner is the one minting", async function () {
+        const { nft, owner, otherAccount } = await loadFixture(deployFixture);
+        const tokenURI = "ipfs://bafkreihm63mue4z3qewc6nhj7ctzaediburwtu2ydr5iid26wgnilbnyhu";
+
+        const initialBalance = await nft.balanceOf(otherAccount.address);
+        expect(initialBalance).to.equal(0);
+
+        const result = nft.connect(owner).safeMint(otherAccount.address, tokenURI);
+        await expect(result).to.not.be.reverted;
+
+        const tokenBalance = await nft.balanceOf(otherAccount.address);
+        expect(tokenBalance).to.equal(1);
     });
+
 
     it("Should throw if owner is not the one minting", async function () {
         const { nft, otherAccount } = await loadFixture(deployFixture);
-        const tokenURI: any = "ipfs://bafkreihm63mue4z3qewc6nhj7ctzaediburwtu2ydr5iid26wgnilbnyhu";
-        expect(await nft.safeMint(otherAccount.address, tokenURI)).to.throw;
+        const tokenURI = "ipfs://bafkreihm63mue4z3qewc6nhj7ctzaediburwtu2ydr5iid26wgnilbnyhu";
+
+        const result = nft.connect(otherAccount).safeMint(otherAccount.address, tokenURI);
+        await expect(result).to.be.revertedWith("Ownable: caller is not the owner");
+
+        const tokenBalance = await nft.balanceOf(otherAccount.address);
+        expect(tokenBalance).to.equal(0);
     });
 });
