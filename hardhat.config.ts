140798e03e153d90aa621973b42a5de05c9eda42
Gaël Blanchemain <anegg0@users.noreply.github.com>
2022-08-08 14:25:48 -0700

    Removed dotenv import

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
 };
