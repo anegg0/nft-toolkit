0b7690a66f8cfa7585f30868ac495e9d8615b1b3
Gaël Blanchemain <anegg0@users.noreply.github.com>
2022-09-08 13:48:04 -0700

    Arbitrary contract address added to contract.ts

--- scripts/contract.ts
+++ scripts/contract.ts
@@ -3,7 +3,7 @@ import { getContractAt } from "@nomiclabs/hardhat-ethers/internal/helpers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import * as dotenv from "dotenv";
 dotenv.config();
-const deployedContratAddress: any = "";
+const deployedContratAddress: any = "0x80D7c973e998c94C7374bAbba6BaFFf7bB260b8F";
 
 export async function getContract(
   { name, hre }: { name: string; hre: HardhatRuntimeEnvironment; }): Promise<Contract> {
