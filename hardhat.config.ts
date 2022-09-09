408d2a72e7c5bf0cb42305e7722422ad2ddc3017
Gaël Blanchemain <anegg0@users.noreply.github.com>
2022-09-09 10:21:50 -0700

    Added Etherscan + accounts in hh config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,21 +2,60 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 import "hardhat-deploy";
-import { utils } from "ethers";
+import "@nomiclabs/hardhat-etherscan";
 import "dotenv/config";
 import "./scripts/mint";
+import { node_url, accounts } from './utils/network';
 
 const privateKey = process.env.PRIVATE_KEY;
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.9',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 2000,
+          },
+        },
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: 0,
+    otherAccount: 1,
+  },
+  etherscan: {
+    apiKey: "WG8G4PBTJDUBBYWA9ZZ56MV3XJXXPNAIH5",
+  },
   networks: {
     hardhat: {
-      accounts: [
-        {
-          privateKey: privateKey as string,
-          balance: utils.parseUnits("1", 36).toString(),
-        },
-      ],
+      accounts: accounts('mainnet'),
+    },
+    staging: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+    production: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
+    },
+    mainnet: {
+      url: node_url('mainnet'),
+      accounts: accounts('mainnet'),
+    },
+    rinkeby: {
+      url: node_url('rinkeby'),
+      accounts: accounts('rinkeby'),
+    },
+    kovan: {
+      url: node_url('kovan'),
+      accounts: accounts('kovan'),
+    },
+    goerli: {
+      url: node_url('goerli'),
+      accounts: accounts('goerli'),
     },
     palm_testnet: {
       url: `https://palm-testnet.infura.io/v3/${process.env.INFURA_API_KEY}` || "",
